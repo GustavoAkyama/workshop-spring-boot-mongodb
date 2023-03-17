627af752ff3fd352ed82fdf9dcb1ec583e96179e
GustavoAkyama <gustavoakyamamga01@gmail.com>
2023-03-16 23:03:19 -0300

    Consulta simples com query methods

--- src/main/java/com/akyama/workshopmongo/resources/PostResource.java
+++ src/main/java/com/akyama/workshopmongo/resources/PostResource.java
@@ -1,16 +1,12 @@
 package com.akyama.workshopmongo.resources;
 
 import com.akyama.workshopmongo.domain.Post;
-import com.akyama.workshopmongo.domain.User;
-import com.akyama.workshopmongo.dto.UserDTO;
+import com.akyama.workshopmongo.resources.util.URL;
 import com.akyama.workshopmongo.services.PostService;
-import com.akyama.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,6 +20,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
 
